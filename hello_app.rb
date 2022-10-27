1b44d4063fe2d8c5ff71849fb244093c3311f8ec
donkennie <ajeigbekehinde160@gmail.com>
2022-10-27 18:52:20 +0100

    intial commit

---
+++ hello_app.rb
@@ -0,0 +1,14 @@
+require 'sinatra'
+require 'json'
+
+get '/' do
+  content_type :json
+  data = 
+  {
+    "slackUsername" => "DonKennie", 
+    "backend" => true,
+    "age" => 20,
+    "bio" => "I'm a passionate and curious person. I Play. I Barb. I Code. I Write"
+  }
+  data.to_json
+end
